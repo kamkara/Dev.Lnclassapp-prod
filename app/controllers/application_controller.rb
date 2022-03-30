37ec929fcaaad46c705db17c0e7ad67130f8de91
kamkara <lnclass.kamkara@gmail.com>
2022-03-30 19:31:03 +0000

    app: add onglet title; user params on applicationcontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    
+    # If you have extra params to permit, append them to the sanitizer.
+    def configure_permitted_parameters
+    sign_up_params          = [ :first_name, :last_name, :full_name, :matricule, :level_name,
+                                :material_name, :school_name, :contact, :city_name, :role, :email,
+                                :password, :slug, :gender, :payment,
+                                :avatar_profil]
+                                
+    #update_params  = [ :school_name, :status, :avatar_profil, :status_payment]
+     
+    devise_parameter_sanitizer.permit :sign_up, keys: sign_up_params
+      devise_parameter_sanitizer.permit :account_update, keys: sign_up_params
+      devise_parameter_sanitizer.permit :sign_in, keys: [:logged, :password]
+    end 
+
+    private
+    
 end
