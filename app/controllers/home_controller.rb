4effbae6643d50e0e8905ff16812ec1cb30f5afa
kamkara <lnclass.kamkara@gmail.com>
2022-03-30 19:48:38 +0000

    add navabar items

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!, except:%i[index]
+
+  #home page
   def index
+    redirect_to feed_path if user_signed_in?
+  end
+
+  #feed page
+  def feed
   end
 end
