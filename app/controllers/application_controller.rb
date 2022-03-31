9f26e37f17d02dcfcfdac81882b7ceb51bee2f9b
kamkara <lnclass.kamkara@gmail.com>
2022-03-31 09:03:42 +0000

    add home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,26 @@
 class ApplicationController < ActionController::Base
 
-    before_action :configure_permitted_parameters, if: :devise_controller?
+  # Add sign up params
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
     
+    #After sign in
+     def after_sign_in_path_for(resource)
+        redirect_to feed_path if: :user_signed_in?
+    end
+     def after_sign_up_path_for(resource)
+        if user_signed_in?
+         feed_path
+        else
+         root_path
+        end
+    end
+  
+
+
+
+
+
     # If you have extra params to permit, append them to the sanitizer.
     def configure_permitted_parameters
     sign_up_params = [ :first_name, :last_name, :full_name, :matricule, :level_name,
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,23 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to:'home#index'
-  get "feed", to:"home#feed"
+  
+  root to:'homepage#index'
+  get "feed", to:'home#index'
+ 
+  ######### USER DATA #########
+  devise_scope :user do
+    get 'profile/edit'    => 'devise/registrations#edit',   :as => :edit_user_registration
+    get 'profile/cancel'  => 'devise/registrations#cancel', :as => :cancel_user_registration
+  end
+
+  devise_for  :users,
+              :path => '',
+              :path_names => 
+              { :sign_in =>     'connexion',
+                :sign_out =>     'logout',
+                :sign_up =>       '',
+                :registration =>  'inscription',
+                :edit =>          'edit'
+              }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
