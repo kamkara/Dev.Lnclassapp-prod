41ab028243a49b12de5f15972ee355bf33d3876c
kamkara <lnclass.kamkara@gmail.com>
2022-03-31 11:15:05 +0000

    add rich text

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,46 @@
 Rails.application.routes.draw do
-  resources :courses
-  resources :materials
-  resources :levels
-  
+ 
   root to:'homepage#index'
   get "feed", to:'home#index'
- 
+  get "team", to:'home#team'
+  
+
+  #Course
+  get "courses-show", to:"courses#show"
+  get "course-list", to:"courses#index"
+  get "lesson", to:"courses#new"
+  
+
+  #Dashboard
+  get "dashboard-admin", to:'dashboard#index'
+  get "setting", to:'dashboard#home'
+  get "new-materials", to:"materials#new"
+  get "new-levels", to:"levels#new"
+  
+  
+  resources :courses do
+    resources :exercices, only: [:new, :create, :show, :index]
+    get "exercices", to:"exercices#index"
+    post '/publish', to: 'exercices#publish'
+  end
+
+
+  resources :exercices, except: [:new, :show, :edit, :create, :update, :destroy, :index] do
+    member do
+      delete 'delete', to: 'exercices#destroy'
+    end
+    resources :questions, only: [:new, :create, :destroy]
+    resources :results, only: [:new, :create]
+  end
+  
+  
+  resources :courses, except: %i[index new ]
+  resources :answers, 
+            :questions,
+            :results,
+            :levels,
+            :materials,
+            :materials, execept: %i[new]
   ######### USER DATA #########
   devise_scope :user do
     get 'profile/edit'    => 'devise/registrations#edit',   :as => :edit_user_registration
