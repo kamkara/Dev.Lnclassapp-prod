586d9165268a9ae654978720dee4f683228a9cd4
kamkara <lnclass.kamkara@gmail.com>
2022-04-06 11:38:39 +0000

    app: add city name

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,14 +1,7 @@
 class HomepageController < ApplicationController
-  before_action :find_citylist
-
-  def index
-    #logger.info("current_user="+current_user.to_json)  
+ 
+  def index 
     redirect_to feed_path if user_signed_in?
   end
 
-  private
-
-  def :find_citylist
-    @city_ereas = CityErea.all
-  end
 end
